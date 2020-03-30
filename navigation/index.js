b7058c93a9ec69c855dacccc3b0668ed9581b8ef
Yousef Alkhalifah <y.alkhalifah@hotmail.com>
2020-03-30 18:21:21 +0300

    import HomeScreen into StackNav

--- navigation/index.js
+++ navigation/index.js
@@ -1,5 +1,5 @@
 import { createAppContainer } from "react-navigation";
-import StackNav from "./StackhNav";
+import StackNav from "./StackNav";
 
 const AppContainer = createAppContainer(StackNav);
 
