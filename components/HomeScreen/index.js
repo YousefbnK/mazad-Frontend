3a3b26e0af51ce532a80b497adfef41a5dee166b
Yousef Alkhalifah <y.alkhalifah@hotmail.com>
2020-03-30 19:39:17 +0300

    Add button to take user to auction when timer finishes

--- components/HomeScreen/index.js
+++ components/HomeScreen/index.js
@@ -1,24 +1,45 @@
 import React, { Component } from "react";
 import CountDown from "react-native-countdown-component";
 
+//Style
+import { View, Text, TouchableOpacity } from "react-native";
+
 //Data
 import categories from "../../categoriesData";
-import { View } from "react-native";
 
 //Components
 import CategoriesList from "../Categories";
 
 class HomeScreen extends Component {
+  state = {
+    timerFinish: false
+  };
+
+  handlePress = () => {
+    if (this.state.timerFinish) {
+      alert("Tadaa");
+    } else {
+      alert("more patience required");
+    }
+  };
   render() {
     const items = categories.map(item => item.name);
     return (
       <View>
+        {!this.state.timerFinish && (
+          <Text>The next Auction is starting in: </Text>
+        )}
         <CountDown
-          until={10}
-          onFinish={() => alert("Tada")}
+          until={5}
+          onFinish={() => this.setState({ timerFinish: true })}
           onPress={() => alert("Hello")}
           size={20}
         />
+        {this.state.timerFinish && (
+          <TouchableOpacity onPress={this.handlePress}>
+            <Text> Take me to the Auction !</Text>
+          </TouchableOpacity>
+        )}
         <CategoriesList items={items} />
       </View>
     );
