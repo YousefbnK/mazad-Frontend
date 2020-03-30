5a07f15e2ef029ae4c31a14a1ecb2b7d9748b6f6
Yousef Alkhalifah <y.alkhalifah@hotmail.com>
2020-03-30 19:10:53 +0300

    create dummy date

---
+++ categoriesData.js
@@ -0,0 +1,19 @@
+const categories = [
+  {
+    id: 1,
+    name: "Misbah",
+    image: "https://picsum.photos/200"
+  },
+  {
+    id: 2,
+    name: "Gems & Stones",
+    image: "https://picsum.photos/200"
+  },
+  {
+    id: 3,
+    name: "Watches",
+    image: "https://picsum.photos/200"
+  }
+];
+
+export default categories;
