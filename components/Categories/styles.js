1c9a833ae4354545c66e661f7293beb262232bf4
Yousef Alkhalifah <y.alkhalifah@hotmail.com>
2020-03-30 19:11:18 +0300

    create grid and its style

---
+++ components/Categories/styles.js
@@ -0,0 +1,19 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  item: {
+    backgroundColor: "grey",
+    margin: 5,
+    width: 100,
+    padding: 8,
+    color: "white"
+  },
+  grid: {
+    marginBottom: 32,
+    marginTop: 16,
+    height: 120,
+    alignItems: "center"
+  }
+});
+
+export default styles;
