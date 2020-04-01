62f762b74d5191665cb39c7fb06ffa50cfcc2efd
Yousef Alkhalifah <y.alkhalifah@hotmail.com>
2020-04-01 21:03:46 +0300

    Edit stacknav for easier testing

--- navigation/StackNav.js
+++ navigation/StackNav.js
@@ -3,14 +3,16 @@ import { createStackNavigator } from "react-navigation-stack";
 // Components
 import HomeScreen from "../components/HomeScreen";
 import AuctionList from "../components/AuctionList";
+import BiddingScreen from "../components/BiddingScreen";
 
 const StackNav = createStackNavigator(
   {
     HomeScreen: HomeScreen,
-    AuctionList: AuctionList
+    AuctionList: AuctionList,
+    BiddingScreen: BiddingScreen
   },
   {
-    initialRouteName: "AuctionList"
+    initialRouteName: "BiddingScreen"
   }
 );
 
