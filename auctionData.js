82dd90deafa07250683f357076a3044363b8d899
Yousef Alkhalifah <y.alkhalifah@hotmail.com>
2020-04-01 18:38:40 +0300

    Create Dummy Data + import into auctionStore

---
+++ auctionData.js
@@ -0,0 +1,34 @@
+const auctions = [
+  {
+    id: 1,
+    name: "Mazadi",
+    startTime: 10
+  },
+  {
+    id: 2,
+    name: "Mazad Muneera",
+    startTime: 30
+  },
+  {
+    id: 3,
+    name: "Mazad Al-Sugoor",
+    startTime: 300
+  },
+  {
+    id: 4,
+    name: "Mazadilicious",
+    startTime: 32
+  },
+  {
+    id: 5,
+    name: "Mazadhum",
+    startTime: 65
+  },
+  {
+    id: 6,
+    name: "Mazad Baitna",
+    startTime: 12000289
+  }
+];
+
+export default auctions;
