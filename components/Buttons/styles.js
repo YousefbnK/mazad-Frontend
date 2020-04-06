6ba20c140621be82626ac011be7655ead76155c1
Yousef Alkhalifah <y.alkhalifah@hotmail.com>
2020-04-06 21:11:29 +0300

    bid button is green only when logged in

--- components/Buttons/styles.js
+++ components/Buttons/styles.js
@@ -14,6 +14,19 @@ const styles = StyleSheet.create({
     bottom: -200,
     left: 20
   },
+  containerAnon: {
+    height: 50,
+    width: 160,
+    borderRadius: 20,
+    borderColor: "grey",
+    borderWidth: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "grey",
+    position: "absolute",
+    bottom: -200,
+    left: 20
+  },
   text: {
     fontSize: 20,
     fontWeight: "bold",
